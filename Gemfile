2b42d6f89fe3ab644a73eddb4a9dfbca9872bd2c
thibaultCha <thibaultcha@me.com>
2014-08-30 22:28:04 -0700

    Random cows querystring for /think and /say

--- Gemfile
+++ Gemfile
@@ -5,4 +5,4 @@ ruby '2.0.0'
 gem 'sinatra'
 gem 'ruby_cowsay'
 gem 'gold_mine'
-gem 'rack-test', require: 'rack/test'
+gem 'rack-test', require: 'rack/test', :groups => [:development, :test]
--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@
 
 require_relative 'lib/FortuneCow.rb'
 
+helpers do
+  def serialize_params(params)
+    {
+      :text => params[:t],
+      :random_cow => params[:r].to_s != ""
+    }
+  end
+end
+
 before do
   content_type 'text/plain'
 end
@@ -12,11 +21,11 @@ get '/' do
 end
 
 get '/think' do
-  FortuneCow.think({ :text => params[:t] })
+  FortuneCow.think(serialize_params(params))
 end
 
 get '/say' do
-  FortuneCow.say({ :text => params[:t] })
+  FortuneCow.say(serialize_params(params))
 end
 
 get '/random' do
